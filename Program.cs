bca7b61c8eb2b0fbc50025fac6c239a06c2e176f
ewdlop <ray810815@gmail.com>
2022-12-27 00:30:34 -0500

    get rid of a comment

--- Program.cs
+++ Program.cs
@@ -155,9 +155,3 @@ public class Policy
 {
     public const string USER_NAME = "UserName";
 }
-
-// Text for tagging
-//string text = "A Part-Of-Speech Tagger (POS Tagger) is a piece of software that reads text"
-//            + "in some language and assigns parts of speech to each word (and other token),"
-//            + " such as noun, verb, adjective, etc., although generally computational "
-//            + "applications use more fine-grained POS tags like 'noun-plural'.";
