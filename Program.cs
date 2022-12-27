bd25224d2a0b614e1c02d2076b35c0dfa7a87c47
ewdlop <ray810815@gmail.com>
2022-12-26 20:43:15 -0500

    update appsetings

--- Program.cs
+++ Program.cs
@@ -78,12 +78,9 @@ builder.Services.AddAuthorizationBuilder()
             
             
 WebApplication app = builder.Build();
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
