0275e9df945456993b1d1bdd840e3ed6dd7ad871
Raymond Lei(雷菩宇) <ray810815@gmail.com>
2025-01-05 23:24:47 -0500

    一袋米要扛几楼

--- RandomAPIApp.AppHost/Program.cs
+++ RandomAPIApp.AppHost/Program.cs
@@ -1,9 +1,13 @@
+using Aspire.Hosting;
+
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.RandomAPIApp>("Projects.RandomAPIApp");
 
 Enumerable.Range(0, 128).ToList().ForEach((int x) =>
 {
-    builder.AddProject<Projects.RandomAPIApp_一袋米要扛几楼>(x);
+    builder.AddProject<Projects.RandomAPIApp_一袋米要扛几楼>("Projects.一袋米要扛几楼")
+        .WithArgs(x.ToString());
 });
 
 builder.Build().Run();
